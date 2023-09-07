9c97e16df939f13ec700b7e2b063e522ded60fc9
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-07 07:54:54 +0530

    RouterProvider and create Browser Router added !

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h2>Home Page</h2>
+  },
+  {
+    path:'/about',
+    element: <h2>About Page</h2>
+  }
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />;
 };
 export default App;
