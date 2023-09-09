a7bb5eb5f83459e26d488cc34c93996773515250
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-09 11:19:38 +0530

    Error page styled component CSS done !

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link, useRouteError } from 'react-router-dom';
 import img from "../assets/not-found.svg";
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
 
 const error = useRouteError();
 if (error.status === 404) {
   return (
-    <>
+    <Wrapper>
       <div className="">
         <img src={img} alt="Not Found" />
         <h3>Ohh !</h3>
         <p>We cant seem to find the page  we are looking for !</p>
         <Link to='/'>back home</Link>
       </div>
-    </>
+    </Wrapper>
   )
 }
 
