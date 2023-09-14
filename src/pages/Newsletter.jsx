61217ec4d859c24f6585ef3ddbdd45d640b36e18
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-14 22:24:40 +0530

    Submit Form

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Form } from 'react-router-dom';
+import { Form, redirect } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 
 export const action = async({request}) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const response = await axios.post(newsletterUrl, data);
 
-  console.log(data)
 
-  return null;
+  console.log(response);
+  toast.success(response.data.msg);
+
+  return redirect('/');
 }
 
 const Newsletter = () => {
@@ -17,17 +23,17 @@ const Newsletter = () => {
       {/* name */}
       <div className="form-row">
         <label htmlFor="name" className='form-label'>name</label>
-        <input type="text" className='form-input' name='name' id='name' defaultValue='john' />
+        <input type="text" className='form-input' name='name' id='name' required />
       </div>
       {/* lastName */}
       <div className="form-row">
         <label htmlFor="lastName" className='form-label'>last name</label>
-        <input type="text" className='form-input' name='lastName' id='lastName' defaultValue='smith' />
+        <input type="text" className='form-input' name='lastName' id='lastName' required />
       </div>
       {/* email */}
       <div className="form-row">
         <label htmlFor="email" className='form-label'>email</label>
-        <input type="text" className='form-input' name='email' id='email' defaultValue='test@test.com' />
+        <input type="text" className='form-input' name='email' id='email' required />
       </div>
       <button className="btn btn-block" type='submit' style={{marginTop: '0.5rem'}}>submit</button>
     </Form>
