250e292cfcc48d4c01023e6d322d64cac78feecf
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-14 22:53:36 +0530

    Search Form structure.

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Wrapper from '../assets/wrappers/SearchForm';
+import { Form, useNavigation } from 'react-router-dom';
 
 const SearchForm = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   return (
-    <div>SearchForm</div>
+    <Wrapper>
+      <Form className='form'>
+        <input type="search" name='search' className='form-input' defaultValue='vodka' />
+        <button disabled={isSubmitting} className="btn" type='submit'>{isSubmitting ? 'submitting' : 'submit'}</button>
+      </Form>
+    </Wrapper>
   )
 }
 
