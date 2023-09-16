fdf0e476e984c48df80aacae67d37bf0cbd53a9f
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-16 22:37:48 +0530

    Redirects and query final setup done

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -2,26 +2,40 @@ import axios from 'axios';
 import React from 'react'
 import Wrapper from "../assets/wrappers/CocktailPage";
 import { Link, Navigate, useLoaderData } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 
 const singleCocktailUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-export const loader = async ({ params }) => {
-  const { id } = params;
-  const data = await axios.get(`${singleCocktailUrl}${id}`);
-  return { id, data };
+const singleCocktailQuery = (id) => {
+  return {
+    queryKey: ['cocktail', id],
+    queryFn: async() => {
+      const data = await axios.get(`${singleCocktailUrl}${id}`);
+      return data;
+    }
+  }
 }
 
+export const loader = (queryClient) =>
+  async ({ params }) => {
+    const { id } = params;
+    await queryClient.ensureQueryData(singleCocktailQuery(id));
+    return { id };
+  }
+
 const Cocktail = () => {
-  const { id, data } = useLoaderData();
+  const { id } = useLoaderData();
+  const {data} = useQuery(singleCocktailQuery(id));
 
   // if(!data.data) return <h2>Something went wrong !</h2>
   if(!data.data) return <Navigate to='/' />
 
   const singleDrink = data.data.drinks[0];
+  console.log(singleDrink);
   const {
-    strDrink: name, strDrinkThumb: image, strAlcohol: info, strCategory: category, strGlass: glass, strInstructions: instructions
+    strDrink: name, strDrinkThumb: image, strAlcoholic: info, strCategory: category, strGlass: glass, strInstructions: instructions
   } = singleDrink;
 
   const validIngredients = Object.keys(singleDrink)
